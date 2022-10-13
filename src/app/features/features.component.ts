4fcb0f7be78eebdab9fec6ff55204eaf66c9ef76
David Alejandro Cuastumal Bucheli <79026475+Dcuastumal@users.noreply.github.com>
2022-10-13 02:14:11 -0500

    removing OnInit with console.logs

--- src/app/features/features.component.ts
+++ src/app/features/features.component.ts
@@ -1,10 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { Component } from '@angular/core';
 
 @Component( {
   selector: 'app-features',
   templateUrl: './features.component.html'
 })
-export class FeaturesComponent implements OnInit {
+export class FeaturesComponent {
 
   public title: string;
 
@@ -12,7 +12,4 @@ export class FeaturesComponent implements OnInit {
     this.title = "Soy el feature component";
   }
 
-  ngOnInit(): void {
-    console.log("Se ha ejecutado el componente features")
-  }
 }
